09109001280b7e03d308f3aabb3e160610808471
Matt <33204743+Laiteux@users.noreply.github.com>
2020-09-17 11:27:31 +1100

    Make TelegramClient.SendAuthenticatedRequestAsync public

--- src/TgSharp.Core/TelegramClient.cs
+++ src/TgSharp.Core/TelegramClient.cs
@@ -266,7 +266,7 @@ namespace TgSharp.Core
             return (T)result;
         }
 
-        internal async Task<T> SendAuthenticatedRequestAsync<T>(TLMethod methodToExecute, CancellationToken token = default(CancellationToken))
+        public async Task<T> SendAuthenticatedRequestAsync<T>(TLMethod methodToExecute, CancellationToken token = default(CancellationToken))
         {
             if (!IsUserAuthorized())
                 throw new InvalidOperationException("Authorize user first!");
