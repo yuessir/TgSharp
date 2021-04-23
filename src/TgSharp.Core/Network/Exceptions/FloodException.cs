b3bb8626bfccb82ba3e26f577dccf4fff494b82d
Matt <matt@laiteux.dev>
2021-04-23 16:12:39 +0200

    Shorten FloodException message

--- src/TgSharp.Core/Network/Exceptions/FloodException.cs
+++ src/TgSharp.Core/Network/Exceptions/FloodException.cs
@@ -7,8 +7,7 @@ namespace TgSharp.Core.Network.Exceptions
         public TimeSpan TimeToWait { get; private set; }
 
         internal FloodException(TimeSpan timeToWait)
-            : base($"Flood prevention. Telegram now requires your program to do requests again only after {timeToWait.TotalSeconds} seconds have passed ({nameof(TimeToWait)} property)." +
-                   " If you think the culprit of this problem may lie in TgSharp's implementation, open a Github issue please.")
+            : base($"Telegram now requires your program to do requests again only after {timeToWait.TotalSeconds} seconds have passed.")
         {
             TimeToWait = timeToWait;
         }
